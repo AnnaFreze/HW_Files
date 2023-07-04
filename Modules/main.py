5fbaf47c78b3aa3e06beb235cb98d0aa6a0cdd36
AnnaFreze <128387326+AnnaFreze@users.noreply.github.com>
2023-07-04 15:06:20 +0300

    Add files via upload

---
+++ Modules/main.py
@@ -0,0 +1,13 @@
+import people
+import salary
+import datetime
+
+
+if __name__ == '__main__':
+    print(people.get_employees(people.candidates))
+    print(salary.calculate_salary(salary.salary_rates))
+    print(datetime.date.today())
+
+
+
+
